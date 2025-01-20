090619875243a148693e212908d74f6f7e5a272d
hsefak <kucuk.hsefa@gmail.com>
2025-01-20 19:39:53 +0300

    Initial Commit

---
+++ UserListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserListApp
+//
+//  Created by Hüseyin Sefa Küçük on 20.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
